1d32da51e215db03c41faad559e58576b036b0b6
Marikit Yake <marikitc@outlook.com>
2024-10-10 17:56:51 -0600

    update forecast scripts

---
+++ scripts/models.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# A streamlined script of our time series forecasting process
+# For time series data that can be characterized by having seasonality and trend.
+from darts.models import Croston, NaiveDrift, NaiveMovingAverage, ExponentialSmoothing
+
+baseline_models = {
+    "Naive": NaiveDrift(),
+    # AR
+    "Moving Average": NaiveMovingAverage(), 
+    "Exponential Smoothing": ExponentialSmoothing()
+}
+
+# Time Series Forecasting Models specializing in data with seasonality and trend.
+st_models = {}
+
+# Time Series Forecasting Models specializing in data with seasonality and negligible trend.
+seasonal_models = {}
+
+# Intermittent Demand Forecasting Models that I can support
+id_models = {
+    "CROSTON Method": Croston(version = "classic"),
+    "SBA": Croston(version = "sba"), # Syntetos-Boylan Approximation
+    "TSB": Croston(version = "tsb") # Teunter-Syntetos-Babai
+}
+
+# Models which I understand support forecasting
+# on data with stationarity.
+auto_models = {
+    # ARIMA
+    # SARIMA
+    # SARIMAX
+    # VARIMA
+    # ETS
+}
